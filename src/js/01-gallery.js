ad0b2dba71845295c54ba19c0c5c76944f7d6910
Vitalii Shtanko <patas323f@gmail.com>
2022-08-28 14:03:49 +0300

    Подключил галерею

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const listGallery = document.querySelector('.gallery');
+
+// Создаем переменную для добавления разметки
+
+const markupGalleryEl = markupGalleryList(galleryItems);
+//console.log(markupGalleryEl)
+
+function markupGalleryList() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class = gallery_lists>
+         <a class = gallery__item href='${original}'>
+      <img
+      class= 'gallery__image'
+      src= '${preview}'
+      
+      alt='${description}'
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+
+// Вставляем разметку в html
+listGallery.insertAdjacentHTML('afterbegin', markupGalleryEl);
+console.log(listGallery);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
